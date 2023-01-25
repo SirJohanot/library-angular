012600707056716917701431fd32c2d884c30f7c
SirJohanot <pavelkrafft@gmail.com>
2023-01-25 14:55:20 +0300

    extended the authentication service

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,28 +1,45 @@
 import { Injectable } from '@angular/core';
 
+const loginKey = "login";
+const passwordKey = "password";
+const rolesKey = "roles";
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
   public getLogin(): string | null {
-    return localStorage.getItem("login");
+    return localStorage.getItem(loginKey);
   }
 
   public getPassword(): string | null {
-    return localStorage.getItem("password");
+    return localStorage.getItem(passwordKey);
   }
 
   public getRoles(): string[] | undefined {
-    return localStorage.getItem("roles")
+    return localStorage.getItem(rolesKey)
       ?.split(",");
   }
 
   public setAuthentication(login: string, password: string, roles?: string[]): void {
-    localStorage.setItem("login", login);
-    localStorage.setItem("password", password);
+    localStorage.setItem(loginKey, login);
+    localStorage.setItem(passwordKey, password);
     if (roles) {
-      localStorage.setItem("roles", roles.join(","));
+      localStorage.setItem(rolesKey, roles.join(","));
     }
+  }
+
+  public resetAuthentication(): void {
+    localStorage.removeItem(loginKey);
+    localStorage.removeItem(passwordKey);
+    localStorage.removeItem(rolesKey);
+  }
+
+  public isAuthenticated(): boolean {
+    const login = localStorage.getItem(loginKey);
+    const password = localStorage.getItem(passwordKey);
+    const roles = localStorage.getItem(rolesKey);
+    return !!(login && password && roles);
   }
 }
