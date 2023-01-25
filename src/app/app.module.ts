378e2e411d9b549bc43a535448809aaf51d3e290
SirJohanot <pavelkrafft@gmail.com>
2023-01-25 14:34:44 +0300

    added the http interceptors to the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { AuthenticationInterceptor } from './interceptors/authentication.interceptor';
+import { BaseUrlInterceptor } from './interceptors/base-url.interceptor';
 import { AddBookComponent } from './pages/add-book/add-book.component';
 import { BooksComponent } from './pages/books/books.component';
 import { OrdersComponent } from './pages/orders/orders.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
-import { UsersComponent } from './pages/users/users.component';
 import { UnauthorizedComponent } from './pages/unauthorized/unauthorized.component';
+import { UsersComponent } from './pages/users/users.component';
 
 @NgModule({
   declarations: [
@@ -36,7 +38,18 @@ import { UnauthorizedComponent } from './pages/unauthorized/unauthorized.compone
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthenticationInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: BaseUrlInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
