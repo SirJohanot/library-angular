05324413141b7e0501dd2315b06fc6f797565cb9
SirJohanot <pavelkrafft@gmail.com>
2023-01-25 15:35:49 +0300

    added a home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationGuard } from './guards/authentication.guard';
 import { AddBookComponent } from './pages/add-book/add-book.component';
 import { BooksComponent } from './pages/books/books.component';
+import { HomeComponent } from './pages/home/home.component';
 import { OrdersComponent } from './pages/orders/orders.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
@@ -21,6 +22,14 @@ const routes: Routes = [
   {
     path: 'unauthorized',
     component: UnauthorizedComponent
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [AuthenticationGuard],
+    data: {
+      expectedRoles: ['READER', 'LIBRARIAN', 'ADMIN']
+    }
   },
   {
     path: 'books',
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  userLogin: string = this.authService.getLogin() || '';
+
+  constructor(private authService: AuthenticationService) { }
+
+}
