93689a10c6b9fd6d4cd31bb87de2ee0ee56530e5
SirJohanot <pavelkrafft@gmail.com>
2023-01-27 10:41:56 +0300

    changed navigation to only render authorized routes

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 import { LanguageService } from 'src/app/services/language.service';
 
 @Component({
@@ -9,7 +10,8 @@ import { LanguageService } from 'src/app/services/language.service';
 export class NavigationComponent {
 
   constructor(
-    public languageService: LanguageService
+    public languageService: LanguageService,
+    public authenticationService: AuthenticationService
   ) { }
 
 }
