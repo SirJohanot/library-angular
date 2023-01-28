e6653e2add5035e755e1276ca61270c89e784cd6
SirJohanot <pavelkrafft@gmail.com>
2023-01-28 11:29:15 +0300

    added Book interface

---
+++ src/app/Book.ts
@@ -0,0 +1,9 @@
+export interface Book {
+    id: number,
+    title: string,
+    authors: { name: string }[],
+    genre: { name: string },
+    publisher: { name: string },
+    publishmentYear: number,
+    amount: number
+}
