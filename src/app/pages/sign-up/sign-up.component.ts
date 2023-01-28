2c1bba8cdc731c6a9eb98fb0325408b93c6da00d
SirJohanot <pavelkrafft@gmail.com>
2023-01-28 09:53:00 +0300

    implemented the sign up page

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, NEVER } from 'rxjs';
+import { LanguageService } from 'src/app/services/language.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,5 +10,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent {
+
+  login: string = '';
+  password: string = '';
+  confirmedPassword: string = '';
+  firstName: string = '';
+  lastName: string = '';
+  error: string = '';
+
+  constructor(
+    public languageService: LanguageService,
+    private http: HttpClient,
+    private router: Router
+  ) { }
+
+  clearError() {
+    this.error = '';
+  }
+
+  handleSubmit() {
+    this.http.post('/users',
+      {
+        login: this.login,
+        password: this.password,
+        confirmedPassword: this.confirmedPassword,
+        firstName: this.firstName,
+        lastName: this.lastName
+      },
+      { observe: 'response' })
+      .pipe(
+        catchError((error) => {
+          switch (error.status) {
+            case 400:
+              this.error = error?.error?.error;
+              break;
+            default:
+              this.error = 'Registration failed';
+          }
+          return NEVER;
+        })
+      )
+      .subscribe(() => {
+        this.router.navigate(['/']);
+      });
+  }
 
 }
