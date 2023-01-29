7de74fd3dd504f74d32c5be402beec414c9d982c
SirJohanot <pavelkrafft@gmail.com>
2023-01-29 13:42:45 +0300

    implemented the add book page

--- src/app/pages/add-book/add-book.component.ts
+++ src/app/pages/add-book/add-book.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { catchError, NEVER } from 'rxjs';
+import { BookDto } from 'src/app/interfaces/BookDto';
+import { LanguageService } from 'src/app/services/language.service';
 
 @Component({
   selector: 'app-add-book',
@@ -7,4 +12,40 @@ import { Component } from '@angular/core';
 })
 export class AddBookComponent {
 
+  book: BookDto = {
+    title: '',
+    authors: '',
+    genre: '',
+    publisher: '',
+    publishmentYear: 2000,
+    amount: 1
+  }
+
+  error: string = '';
+
+  constructor(
+    public languageService: LanguageService,
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private router: Router
+  ) { }
+
+  handleSubmit(book: BookDto) {
+    this.http.post("/books", book, { observe: 'response' })
+      .pipe(
+        catchError((error) => {
+          switch (error.status) {
+            case 400:
+              this.error = error?.error?.error;
+              break;
+            default:
+              this.error = 'Could not add book';
+          }
+          return NEVER;
+        })
+      )
+      .subscribe(() => {
+        this.router.navigate(["/books"]);
+      });
+  }
 }
