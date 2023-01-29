61f22fd1f3a27bdaeed3e31004029f370d0b6789
SirJohanot <pavelkrafft@gmail.com>
2023-01-29 13:42:19 +0300

    implemented the delete button in the book page

--- src/app/pages/book/book.component.ts
+++ src/app/pages/book/book.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Book } from 'src/app/interfaces/Book';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { LanguageService } from 'src/app/services/language.service';
@@ -19,13 +19,19 @@ export class BookComponent implements OnInit {
     public languageService: LanguageService,
     public authenticationService: AuthenticationService,
     private route: ActivatedRoute,
-    private http: HttpClient
+    private http: HttpClient,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
     this.bookId = +(this.route.snapshot.paramMap.get('id') || 0);
     this.http.get<Book>(`/books/${this.bookId}`)
       .subscribe(book => this.book = book);
+  }
+
+  handleDelete(): void {
+    this.http.delete(`/books/${this.bookId}`)
+      .subscribe(() => this.router.navigate(['/books']));
   }
 
 }
