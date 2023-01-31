d85d81a7caf1995a3f2e25087c143ccf11bbacd4
SirJohanot <pavelkrafft@gmail.com>
2023-01-31 12:21:21 +0300

    implemented the Orders page

--- src/app/pages/orders/orders.component.ts
+++ src/app/pages/orders/orders.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { Order } from 'src/app/interfaces/Order';
 
 @Component({
   selector: 'app-orders',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./orders.component.css']
 })
 export class OrdersComponent {
+
+  allOrders!: Order[];
+  displayedOrders!: Order[];
+
+  constructor(
+    private http: HttpClient,
+    private ref: ChangeDetectorRef
+  ) { }
+
+  ngOnInit(): void {
+    this.http.get<Order[]>("/orders")
+      .subscribe(orders => {
+        this.allOrders = orders;
+        this.ref.detectChanges()
+      });
+  }
+
+  setDisplayedOrders(orders: Order[]): void {
+    this.displayedOrders = orders;
+  }
+
+  trackOrders(index: number, order: Order) {
+    return order.id;
+  }
 
 }
