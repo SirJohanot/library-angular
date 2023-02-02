9f28507c1b62ac7c36eec75b23afd93d9476454b
SirJohanot <pavelkrafft@gmail.com>
2023-02-02 10:02:05 +0300

    implemented the Users page

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { User } from 'src/app/interfaces/User';
 
 @Component({
   selector: 'app-users',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent {
+
+  allUsers!: User[];
+  displayedUsers!: User[];
+
+  constructor(
+    private http: HttpClient,
+    private ref: ChangeDetectorRef
+
+  ) { }
+
+  ngOnInit(): void {
+    this.http.get<User[]>("/users")
+      .subscribe(users => {
+        this.allUsers = users;
+        this.ref.detectChanges()
+      });
+  }
+
+  setDisplayedUsers(users: User[]): void {
+    this.displayedUsers = users;
+  }
+
+  trackUsers(index: number, user: User) {
+    return user.id;
+  }
 
 }
